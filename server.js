18df7953711c992d9cf9d749f665000a30056f79
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-10-05 08:53:23 -0400

    update to switch branches

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { parks } = require('./data/parks.json')
 
 const app = express();
+
+app.get('/api/parks', (req,res) => {
+    res.json(parks);
+})
 
 
 app.listen(3001, () =>{
