326d44f63e8edaadaf028aef9816d44fa3667639
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-10-07 21:54:24 -0400

    bug with filterByQueryFunction -- function called but does not filter data

--- server.js
+++ server.js
@@ -3,7 +3,27 @@ const { parks } = require('./data/parks.json')
 
 const app = express();
 
+function filterByQuery(query, resultsArr) {
+    let filteredResults = resultsArr;
+    if (query.id) {
+        filteredResults = filteredResults.filter(park => park.id === query.id);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(park => park.name === query.name);
+    }
+    if (query.location) {
+        filteredResults = filteredResults.filter(park => park.location === query.location);
+        console.log('function called');
+    }
+   return filteredResults;
+}
+
 app.get('/api/parks', (req,res) => {
+    let results = parks;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+        console.log(req.query);
+    }
     res.json(parks);
 })
 
